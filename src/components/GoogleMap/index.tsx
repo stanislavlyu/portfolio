58dd60ddff99234758d23b6aba67ef81c487ba16
stanislavlyu <stas-lyu@outlook.com>
2025-01-07 18:52:34 +0800

    add google maps

--- src/components/GoogleMap/index.tsx
+++ src/components/GoogleMap/index.tsx
@@ -2,27 +2,24 @@ import Container from '@components/Container'
 import Footer from '@components/Footer'
 import Section from '@components/Section'
 import Title from '@components/Title'
+import { GoogleMapsEmbed } from '@next/third-parties/google'
+import { GOOGLE_MAPS_API_KEY } from './constants'
 
 const GoogleMap = () => {
 	return (
 		<Section id='location' className='!pb-0'>
-			<Container>
+			<Container className='mb-4 md:mb-16'>
 				<Title
 					title="I'm Located in Bali"
 					text="'Resident of Great Britain and Ukraine, currently based on the vibrant and serene island of Bali. Drawing inspiration from its unique culture and environment while staying connected with the global community for remote collaborations.'"
 				/>
 			</Container>
-			<iframe
-				suppressHydrationWarning
-				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d591311.1232301384!2d114.74192590302222!3d-8.454654167577194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd141d3e8100fa1%3A0x24910fb14b24e690!2sBali!5e1!3m2!1sen!2sid!4v1735915662383!5m2!1sen!2sid'
+			<GoogleMapsEmbed
+				apiKey={GOOGLE_MAPS_API_KEY}
 				width='100%'
-				height='450'
-				className='mt-16 border-none'
-				allowFullScreen
-				loading='lazy'
-				referrerPolicy='no-referrer-when-downgrade'
-				title='Location map'
-				tabIndex={0}
+				height={450}
+				mode='place'
+				q='Bali'
 			/>
 			<Footer />
 		</Section>
