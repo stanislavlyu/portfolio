337a8e8bab2107ce135596a70d867bc4e775c04d
stanislavlyu <stas-lyu@outlook.com>
2025-01-11 20:35:50 +0800

    adjust animation

--- src/components/Hero/ThreeBackgroud/index.tsx
+++ src/components/Hero/ThreeBackgroud/index.tsx
@@ -6,7 +6,7 @@ import { useThreeBackground } from './hooks/useThreeBackground'
 const ThreeBackground: React.FC = () => {
 	const containerRef = useRef<HTMLDivElement | null>(null)
 	useThreeBackground(containerRef)
-	return <div ref={containerRef} className='absolute left-0 top-0 -z-[1] h-full w-full' />
+	return <div ref={containerRef} className='fixed left-0 top-0 -z-[1] h-full w-full' />
 }
 
 export default ThreeBackground
