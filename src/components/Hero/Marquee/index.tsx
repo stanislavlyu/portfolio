6ace0ce983ebbe780a4894c32721dacb3f003fe7
stanislavlyu <stas-lyu@outlook.com>
2025-01-18 23:04:23 +0800

    Add full width style to HeroMarquee slider
    Set the HeroMarquee slider's width to 100% to ensure it spans the full container. This enhances responsiveness and maintains a consistent layout across various screen sizes.

--- src/components/Hero/Marquee/index.tsx
+++ src/components/Hero/Marquee/index.tsx
@@ -5,7 +5,7 @@ import { TECH_ICONS } from './constants'
 
 const HeroMarquee = () => {
 	return (
-		<InfiniteSlider className='absolute bottom-6 z-10' gap={24} duration={50} reverse>
+		<InfiniteSlider className='absolute bottom-6 z-10 w-full' gap={24} duration={50} reverse>
 			{TECH_ICONS.map(({ title, icon, alt }, index) => (
 				<div
 					key={index}
